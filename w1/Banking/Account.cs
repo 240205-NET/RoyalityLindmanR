edf0fc490caeebebae0587e0ea1424e96bb1b24d
Russell <russellxy@uri.edu>
2024-02-07 14:12:57 -0500

    "wk1 folder adding again"

--- w1/Banking/Account.cs
+++ w1/Banking/Account.cs
@@ -1,60 +1,148 @@
-namespace Banking {
-    class Account {
-        //fields
-            // pin
-            // account owner
+namespace Banking
+{
+    abstract class Account
+    {
+    // fields (properties and state)
+        // pin
+        // account owner
         private string owner;
-            // type of account
-            // account number
-            
-        
-        public int accountNumber { get; }
+        // type (checking/savings)
+        // account number 
 
-        public void setAccountNuber(int n) {
-            if(n == null) {
-                throw new Exception(MessageProcessingHandler: "value cannot be null", paramName :nameof(ValueTask))
-            }
-            this.accountNumber = n;
-        }
+        // transaction history
+        // [access modifier] [modifier] [type] [name]
+        // List<T> ListName
+        private List<Transaction> transactions = new List<Transaction>();
+
+
+        // Michael asked, so here you go...
+        // account -> savings account
+        // savings "is a " account
+        // child "is a" parent
+        // List<account> accounts = new List<savings>
+        // works for direct inheritance, and Interfaces
+
+        // now back to our regularly programmed class...
+
+        
+        // Access Modifiers
+            // public
+            // private - * default access modifier
+            // protected
+            // internal 
+
+        
+        //getter/setter - special methods that allow you to modify the value of a field
+            // private int accountNumber 
+            // public int getAccountNumber()
+            // {
+            //     return this.accountNumber;
+            // }
+            // public void setAccountNumber(int number)
+            // {
+            //     this.accountNumber = number;
+            // }
+
+        public int accountNumber { get; set; }
+
+        // public void setAccountNumber( int number)
+        // { 
+        //     if (number == null)
+        //     {
+        //         throw new Exception(MessageProcessingHandler: "value cannot be null", paramName :nameof(ValueTask));
+        //     }
+        //     this.accountNumber = value;
+        // }
 
         // balance
         protected double balance;
+
+        private static int accountNumberSeed = 123;
         
-    private static int accountNumberSeed = 123;
-
-        //constructors
-    public  Account(string ownersName, double initialBalance) {
+    // constructor - a specific method that details how to make an Account object.
+        public Account(string ownerName, double initialBalance)
+        { 
             //create account num
+            this.accountNumber = accountNumberSeed;
+            accountNumberSeed++;
 
-            this.accountNumber = this.accountNumberSeed;
-            this.accountNumberSeed++;
-            this.owner = ownersName;
+            this.owner = ownerName;
             MakeDeposit(initialBalance);
-    }
+        }
 
-    public  Account(string ownersName) {
+        // Overloading - two methods with the same name, with two different functionalities
+        public Account(string ownerName)
+        { 
             //create account num
+            this.accountNumber = accountNumberSeed;
+            accountNumberSeed++;
 
-            this.accountNumber = this.accountNumberSeed;
-            this.accountNumberSeed++;
-            this.owner = ownersName;
+            this.owner = ownerName;
             MakeDeposit(0);
-    }
-        
+        }
 
-        //methods
+    // methods (behavior)
 
-            //withdrawls
-            //balance
-            //deposits
+        // deposits
+        public void MakeDeposit (double amount, string note = "")
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("deposit may not be less than 0.");
+            }
+            else
+            {
+                balance += amount;
+                Transaction deposit = new Transaction(amount, DateTime.Now, note);
+                transactions.Add(deposit);
+            }
+        }
 
-            //associate with another account
-            //transfer
-            //closing account (remove account info)
+        // withdrawls
+        public virtual void MakeWithdrawl (double amount, string note = "")
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdrawls may not be less than 0.");
+            }
+            else if (balance-amount < 0)
+            {
+                Console.WriteLine("Insuffiecient funds!");
+            }
+            else
+            {
+                balance -= amount;
+                Transaction withdrawl = new Transaction(-(amount), DateTime.Now, note);
+                transactions.Add(withdrawl);
+            }
+        }
+
+        // abstract methods MUST be overridden by a derived class
+        // virtual methods CAN be overridden by a derived class, but don't have to.
+
+        // account balance
+        public virtual string DisplayBalance()
+        {
+            string balanceString = "Account # " + this.accountNumber + " has a balance of: " + this.balance;
+            return balanceString;
+        }
+
+        // display the transaction history
+        public virtual string DisplayTransactionHistory()
+        {
+            var history = new System.Text.StringBuilder();
+
+            history.AppendLine("Date\t\tAmount\t\tNote");// \n - new line  \t - tab-space
+
+            foreach(Transaction item in transactions)
+            {
+                history.AppendLine($"{item.date.ToShortDateString()}\t{item.amount}\t{item.note}"); // \n
+            }
+            return history.ToString();
+        }
+
+        // associate with another account
+        // transfer to other accounts
+        // closing account (remove account info)
     }
 }
-
-            //account
-                
-                
-                
--- w1/Banking/Program.cs
+++ w1/Banking/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 
-namespace Banking {
-    class Program {
-        public static void Main(string[] args) {
-            
-            
-            
-            
-            
-        }
-    }
+namespace Banking
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Account firstAccount = new SavingsAccount("James", 1000);
+            Account secondAccount = new SavingsAccount("Randall", 1000);
+            Account thirdAccount = new SavingsAccount("Meryem", 1000);
 
+            Console.WriteLine(firstAccount.DisplayBalance());
+            Console.WriteLine(secondAccount.DisplayBalance());
+            Console.WriteLine(thirdAccount.DisplayBalance());
+
+            secondAccount.MakeDeposit(500);
+
+            Console.WriteLine(secondAccount.DisplayBalance());
+
+            Console.WriteLine(secondAccount.DisplayTransactionHistory());
+        }
+
+    }
 }
