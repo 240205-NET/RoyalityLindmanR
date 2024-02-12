1fc6d60a1b5599faeeee16b36452df093b0b3783
Russell <russellxy@uri.edu>
2024-02-12 18:19:01 -0500

    added 10 tests, need to read and write to file

--- p1/RockPaperScissors.Test/UnitTest1.cs
+++ p1/RockPaperScissors.Test/UnitTest1.cs
@@ -21,6 +21,18 @@ namespace RockPaperScissors.Test
         }
 
         [Fact]
+        public void CheckCpuChoiceType()
+        {
+            //Arrange
+            Cpu computer = new Cpu();
+
+            //Act
+
+            //Assert
+            Assert.IsType<int>(computer.playGame());
+        }
+
+        [Fact]
         public void CheckCpuChoiceNumber()
         {
             //Arrange
@@ -54,6 +66,18 @@ namespace RockPaperScissors.Test
             //Assert
             Assert.NotNull(computer);
         }
+
+        [Fact]
+        public void GameNotNull()
+        {
+            //Arrange
+            Game game = new Game();
+
+            //Act
+
+            //Assert
+            Assert.NotNull(game);
+        }
         
          [Fact]
         public void CheckPlayerOptionsType()
@@ -66,6 +90,52 @@ namespace RockPaperScissors.Test
             //Assert
             Assert.IsType<int>(newUser.playGame(1));
         }
+         
+         [Fact]
+        public void CheckPlayerOptions1()
+        {
+            //Arrange
+            User newUser = new User("test dummy");
+            int test = 1;
+
+            //Act
+            int result = newUser.playGame(test);
+
+
+            //Assert
+            Assert.Equal(test, result);
+        }
+         
+         [Fact]
+        public void CheckPlayerOptions2()
+        {
+            //Arrange
+            User newUser = new User("test dummy");
+            int test = 2;
+
+            //Act
+            int result = newUser.playGame(test);
+
+
+            //Assert
+            Assert.Equal(test, result);
+        } 
+        
+        [Fact]
+        public void CheckPlayerOptions3()
+        {
+            //Arrange
+            User newUser = new User("test dummy");
+            int test = 3;
+
+            //Act
+            int result = newUser.playGame(test);
+
+
+            //Assert
+            Assert.Equal(test, result);
+        }
+
     }
 }
 
